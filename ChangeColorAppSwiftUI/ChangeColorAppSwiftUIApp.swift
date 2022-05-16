d647e87614bd082bfd08ad641203590e2bc12d00
maximsamus <maximsamus1@gmail.com>
2022-05-16 13:28:28 +0300

    Initial Commit

---
+++ ChangeColorAppSwiftUI/ChangeColorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeColorAppSwiftUIApp.swift
+//  ChangeColorAppSwiftUI
+//
+//  Created by Максим Самусь on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChangeColorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
