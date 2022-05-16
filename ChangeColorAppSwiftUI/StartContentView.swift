bb18d92f4662e0404de85409e2c4c27c4b55966c
maximsamus <maximsamus1@gmail.com>
2022-05-16 21:15:24 +0300

    UI fix

--- ChangeColorAppSwiftUI/StartContentView.swift
+++ ChangeColorAppSwiftUI/StartContentView.swift
@@ -12,21 +12,24 @@ struct StartContentView: View {
     @State private var redSliderValue = 38.0
     @State private var greenSliderValue = 99.0
     @State private var blueSliderValue = 222.0
+    @State private var redTextFieldValue = "38"
+    @State private var greenTextFieldValue = "99"
+    @State private var blueTextFieldValue = "222"
     
     var body: some View {
         VStack {
-//            ZStack {
-//                Color(.blue)
-//                    .ignoresSafeArea()
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(color)
-                    .frame(width: 300, height: 200)
-                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white, lineWidth: 8))
-
+            //            ZStack {
+            //                Color(.blue)
+            //                    .ignoresSafeArea()
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(color)
+                .frame(width: 300, height: 200)
+                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white, lineWidth: 8))
+            
                 .padding(EdgeInsets(top: 20, leading: 16, bottom: 20, trailing: 16))
-               
-            ColorSliderView(value: $redSliderValue, sliderColor: .red);            ColorSliderView(value: $greenSliderValue, sliderColor: .green);
-            ColorSliderView(value: $blueSliderValue, sliderColor: .blue)
+            
+            ColorSliderView(value: $redSliderValue, sliderColor: .red);          ColorSliderView(value: $greenSliderValue, sliderColor: .green);
+            ColorSliderView(value: $blueSliderValue,  sliderColor: .blue)
             
             
             Spacer()
@@ -50,6 +53,9 @@ struct ColorSliderView: View {
             Text("\(lround(value))")
             Slider(value: $value, in: 0...255, step: 1)
                 .accentColor(sliderColor)
+            TextField("", value: $value, formatter: NumberFormatter())
+                .frame(width: 40, height: 40)
+                
         }
         .padding(.horizontal)
     }
